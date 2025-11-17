65c168238207e4ead5be168eb7697dd929e2f0fa
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-11-17 14:19:47 +0100

    feat(hyprtracker): also log failNotif to log file

--- plugin/src/hyprtracker.cpp
+++ plugin/src/hyprtracker.cpp
@@ -17,103 +17,122 @@ std::chrono::time_point<std::chrono::system_clock> switch_time =
 
 APICALL EXPORT std::string PLUGIN_API_VERSION() { return HYPRLAND_API_VERSION; }
 
-static void failNotif(const std::string &reason) {
-  HyprlandAPI::addNotification(
-      PHANDLE, "[hyprtracker] Failure in initialization: " + reason,
-      CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+static void failNotif(const std::string& reason)
+{
+    std::cerr << "Failure: " << reason << std::endl;
+    HyprlandAPI::addNotification(
+        PHANDLE, "[hyprtracker] Failure: " + reason,
+        CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
 }
 
-bool make_storage_available() {
-  auto homePath = std::getenv("HOME");
-  if (homePath == nullptr) {
-    return false;
-  }
-  const auto p = homePath + STORAGE_LOCATION;
-  if (auto fp = p + "/storage.db"; std::filesystem::exists(fp)) {
+bool make_storage_available()
+{
+    auto homePath = std::getenv("HOME");
+    if (homePath == nullptr)
+    {
+        return false;
+    }
+    const auto p = homePath + STORAGE_LOCATION;
+    if (auto fp = p + "/storage.db"; std::filesystem::exists(fp))
+    {
+        return true;
+    }
+    if (!std::filesystem::exists(p) && !std::filesystem::create_directories(p))
+    {
+        return false;
+    }
     return true;
-  }
-  if (!std::filesystem::exists(p) && !std::filesystem::create_directories(p)) {
-    return false;
-  }
-  return true;
 }
 
-void redirect_outputs() {
-  log_file = std::make_unique<std::ofstream>("/tmp/hyprtracker.log");
+void redirect_outputs()
+{
+    log_file = std::make_unique<std::ofstream>("/tmp/hyprtracker.log");
 
-  if (!(*log_file).is_open()) {
-    std::cerr << "Failed to open log file" << std::endl;
-    throw std::runtime_error("Failed to open log file");
-  }
+    if (!(*log_file).is_open())
+    {
+        std::cerr << "Failed to open log file" << std::endl;
+        throw std::runtime_error("Failed to open log file");
+    }
 
-  // Redirect cout to the file by setting cout's buffer to the file's buffer
-  std::cout.rdbuf((*log_file).rdbuf());
-  std::cerr.rdbuf((*log_file).rdbuf());
-  std::cout << "Hyprtracker logging initiated" << std::endl;
+    // Redirect cout to the file by setting cout's buffer to the file's buffer
+    std::cout.rdbuf((*log_file).rdbuf());
+    std::cerr.rdbuf((*log_file).rdbuf());
+    std::cout << "Hyprtracker logging initiated" << std::endl;
 }
 
-void handle_window_change(const std::string &window_title) {
-  if (window_title == active_window)
-    return;
+void handle_window_change(const std::string& window_title)
+{
+    if (window_title == active_window)
+        return;
 
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>(
-                      std::chrono::system_clock::now() - switch_time)
-                      .count();
-  switch_time = std::chrono::system_clock::now();
-  store_ptr->storeWindowChange(active_window, duration);
-  active_window = window_title;
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now() - switch_time)
+        .count();
+    switch_time = std::chrono::system_clock::now();
+    store_ptr->storeWindowChange(active_window, duration);
+    active_window = window_title;
 }
 
-APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
-  PHANDLE = handle;
+APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
+{
+    PHANDLE = handle;
 
-  const std::string HASH = __hyprland_api_get_hash();
+    const std::string HASH = __hyprland_api_get_hash();
 
-  if (HASH != GIT_COMMIT_HASH) {
-    failNotif(
-        "Version mismatch (headers ver is not equal to running hyprland ver)");
-    throw std::runtime_error("[ht] Version mismatch");
-  }
+    if (HASH != GIT_COMMIT_HASH)
+    {
+        failNotif(
+            "Version mismatch (headers ver is not equal to running hyprland ver)");
+        throw std::runtime_error("[ht] Version mismatch");
+    }
 
-  redirect_outputs();
+    redirect_outputs();
 
-  if (!make_storage_available()) {
-    failNotif("Unable to create/check storage directory, make sure the HOME "
-              "variable is set");
-    throw std::runtime_error("[ht] storage initialization error");
-  }
+    if (!make_storage_available())
+    {
+        failNotif("Unable to create/check storage directory, make sure the HOME "
+            "variable is set");
+        throw std::runtime_error("[ht] storage initialization error");
+    }
 
-  store_ptr = std::make_shared<Store>();
-  store_ptr->init();
+    store_ptr = std::make_shared<Store>();
+    store_ptr->init();
 
-  static auto P = HyprlandAPI::registerCallbackDynamic(
-      PHANDLE, "activeWindow",
-      [&](void *self, SCallbackInfo &info, std::any data) {
-        try {
-          auto window = std::any_cast<PHLWINDOW>(data);
-          auto title = window->m_class;
-          if (title.empty()) {
-            title = window->m_title;
-          }
-          // auto title = window->m_initialTitle;
-          // if (title.empty())
-          // {
-          //     title = window->m_initialClass;
-          // }
-          handle_window_change(title);
-        } catch (const std::bad_any_cast &e) {
-          std::cerr << "casting exception: " << e.what() << std::endl;
-          throw std::runtime_error(
-              "[ht] failed to parse data from window change event");
-        }
-      });
+    static auto P = HyprlandAPI::registerCallbackDynamic(
+        PHANDLE, "activeWindow",
+        [&](void* self, SCallbackInfo& info, std::any data)
+        {
+            try
+            {
+                auto window = std::any_cast<PHLWINDOW>(data);
+                auto title = window->m_class;
+                if (title.empty())
+                {
+                    title = window->m_title;
+                }
+                // auto title = window->m_initialTitle;
+                // if (title.empty())
+                // {
+                //     title = window->m_initialClass;
+                // }
+                handle_window_change(title);
+            }
+            catch (const std::bad_any_cast& e)
+            {
+                std::cerr << "casting exception: " << e.what() << std::endl;
+                throw std::runtime_error(
+                    "[ht] failed to parse data from window change event");
+            }
+        });
 
-  return {"hyprtracker", "A time tracking plugin", "NuttyShrimp", "1.0"};
+    return {"hyprtracker", "A time tracking plugin", "NuttyShrimp", "1.0"};
 }
 
-APICALL EXPORT void PLUGIN_EXIT() {
-  handle_window_change("");
-  if (log_file) {
-    log_file->close();
-  }
+APICALL EXPORT void PLUGIN_EXIT()
+{
+    handle_window_change("");
+    if (log_file)
+    {
+        log_file->close();
+    }
 }
