46fe248dc99ddd978e0e9b8a4ccf55394cf1f05c
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-11-24 12:06:28 +0100

    feat(viewer): add adwaita + other changes

--- viewer/src/main.cpp
+++ viewer/src/main.cpp
@@ -1,17 +1,16 @@
+#include "adwaita.h"
+#include "gio/gio.h"
 #include "viewer.h"
-#include "gtkmm-4.0/gtkmm.h"
 
-
-int main(int argc, char* argv[])
-{
-    auto app = Gtk::Application::create("org.nuttyshrimp.hyprtracker.viewer");
-    app->signal_activate().connect([app]()
-    {
-        auto window = new Viewer();
-        window->init();
-        app->add_window(*window);
-        window->present();
-    });
-    app->run(argc, argv);
-    return 0;
+int main(int argc, char *argv[]) {
+  auto app = adw_application_new("org.nuttyshrimp.hyprtracker.viewer",
+                                 G_APPLICATION_DEFAULT_FLAGS);
+  app->signal_activate().connect([app]() {
+    auto window = new Viewer();
+    window->init();
+    app->add_window(*window);
+    window->present();
+  });
+  app->run(argc, argv);
+  return 0;
 }
--- viewer/src/viewer.h
+++ viewer/src/viewer.h
@@ -4,23 +4,23 @@
 
 #ifndef VIEWER_VIEWER_H
 #define VIEWER_VIEWER_H
+#include <adwaita.h>
 #include <gtkmm/applicationwindow.h>
 #include <gtkmm/label.h>
+#include <memory>
 
 #include "state.h"
 
-
-class Viewer : public Gtk::ApplicationWindow
-{
+class Viewer : public Gtk::ApplicationWindow {
 public:
-    Viewer();
-    void init();
+  Viewer();
+  void init();
 
 private:
-    void create_date_selector();
-    void set_week_label();
-    std::shared_ptr<::State> m_state;
-    Gtk::Label m_week_label;
+  void create_date_selector();
+  void set_week_label();
+  std::shared_ptr<::State> m_state;
+  Gtk::Label m_week_label;
 };
 
-#endif //VIEWER_VIEWER_H
+#endif // VIEWER_VIEWER_H
